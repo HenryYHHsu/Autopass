3648d5b166aa420449968c09dbff1f4e4a501ce4
HenryYHHsu <henryhsu0529@gmail.com>
2020-02-19 14:08:21 +0800

    Add actives controller

---
+++ app/controllers/actives_controller.rb
@@ -0,0 +1,66 @@
+class ActivesController < ApplicationController
+
+    skip_before_action :verify_authenticity_token
+    
+    def create
+        active_name = params.require(:name)
+        condition = params.require(:condition) #  active_condition
+        content = params.require(:content)     #  active_content 
+        for i in 0..(condition.length-1)                  
+            case  condition[i][:symbol]
+            when 0
+                condition_a_status = condition[i][:status]
+                condition_a_target = condition[i][:values][0]
+                if (condition[i][:condition] == true)
+                    condition_a_type = condition[i][:condition]
+                else
+                    condition_a_type = condition[i][:condition]
+                    condition_a_limit = condition[i][:values][1]
+                end
+            when 1
+                condition_b_status = condition[i][:status]
+                condition_b_target = condition[i][:values][0]
+                if (condition[i][:condition] == true)
+                    condition_b_type = condition[i][:condition]
+                else
+                    condition_b_type = condition[i][:condition]
+                    condition_b_limit = condition[i][:values][1]
+                end
+            when 2
+                condition_c_status = condition[i][:status]
+                condition_c_start  = condition[i][:values][0].to_time
+                condition_c_end  = condition[i][:values][1].to_time
+            when 3
+                condition_d_status = condition[i][:status]
+                condition_d_target  = condition[i][:values]
+            when 4
+                condition_e_status = condition[i][:status]
+                condition_e_target  = condition[i][:values]
+            else
+            end
+        end
+        for i in 0..(content.length-1)
+            case  content[i][:symbol]
+            when 0
+                
+            when 1
+                puts "1"
+            when 2
+                puts "2"
+            when 3
+                puts "3"
+            when 4
+                puts "4"
+            else
+            end
+        end
+        puts "-----------------------------"
+        puts active_name
+        puts "-----------------------------"
+        puts condition
+        puts "-------------------------------"
+        puts content
+        puts "-------------------------------"
+        render json: {foodlike: false}
+    end
+end
--- app/javascript/packs/active.js
+++ app/javascript/packs/active.js
@@ -32,8 +32,8 @@ document.addEventListener('turbolinks:load', () => {
           money_discount_cash:0,
           money_discount_percent:0,
           free_delivery_target:0,
-          time: new moment().format('l'),
-          range: [ new moment().format('l'), new moment().format('l')],
+          time: new Date().getTime(),
+          range: [new Date().getTime(), new Date().getTime()],
           time_delta:0,
           store_name:"",
           item_name:"",
@@ -73,34 +73,32 @@ document.addEventListener('turbolinks:load', () => {
         create_active: function(){
           this.activestatus =true;
           let active_condition = [{ name: '活動總數量',
+                                    symbol:0,
                                     status: this.checked_single,
-                                    condtion:this.singleactivestatus,
-                                    description:'優惠上限金額',
-                                    values: this.single_number,
-                                    limit: this.single_limit_number
+                                    condition:this.singleactivestatus,
+                                    values: [this.single_number,this.single_limit_number],
                                   },
                                   {
                                     name: '每人限定次數',
+                                    symbol:1,
                                     status: this.checked_user,
-                                    condtion: this.useractivestatus,
-                                    description: '優惠上限金額',
-                                    values: this.user_number,
-                                    limit: this.user_limit_number
+                                    condition: this.useractivestatus,
+                                    values: [this.user_number,this.user_limit_number]
                                   },
                                   { name: '期限限定期間',
+                                    symbol:2,
                                     status:this.checked_time,
-                                    values:this.range,
-                                    condtion:true,
+                                    values: [this.range[0],this.range[1]]
                                   },
                                   { name: '特定店活動(店名)',
+                                    symbol:3,
                                     status:this.checked_ps,
-                                    values: this.store_name,
-                                    condtion:true,
+                                    values: [this.store_name],
                                   },
                                   { name: '特定產品活動(產品編號)',
+                                    symbol:4,
                                     status: this.checked_pi,
-                                    values: this.item_name,
-                                    condtion:true,
+                                    values: [this.item_name],
                                   }]
           this.active_condition_content =[];
           let active_condition_sort = active_condition.filter( function(item, index, array){
@@ -114,41 +112,53 @@ document.addEventListener('turbolinks:load', () => {
           }
         },
         create_content: function(){
-          if(this.activestatus === true){
-          let active_content_select = [{ name: '活動A:滿額折現',
+          let active_content_select =[{ name: '活動A:滿額折現',
                                         status: this.checked_a,
+                                        symbol: 0,
                                         content: this.money_discount,
                                         value:[this.money_discount_target,this.money_discount_cash,this.money_discount_percent]
                                       },
                                       { name: '活動B:滿額免運',
                                         status: this.checked_b,
-                                        value:this.free_delivery_target
+                                        symbol: 1,
+                                        value:[this.free_delivery_target]
                                       },
                                       { name: '活動C:滿額送禮',
                                         status: this.checked_c,
-                                        value: this.extra_bonus_target
+                                        symbol: 2,
+                                        value: [this.extra_bonus_target]
                                       },
                                       { name: '活動D:特定店優惠',
                                         status: this.checked_d,
+                                        symbol:3,
                                         content: this.ps,
                                         value:[this.ps_target, this.ps_cash, this.ps_percent]
                                       },
                                       { name: '活動E:特定商品優惠',
                                         status: this.checked_e,
+                                        symbol:4,
                                         content: this.pi,
                                         value:[this.pi_target, this.pi_cash, this.pi_percent]
                                       }]     
           this.active_content= active_content_select.filter( function(item, index, array){
-              return item.status === true
+          return item.status === true
           })
+          if(this.activestatus === true && this.active_content.length > 0 ){
           let active_hash = {
               name: this.activename,
               condition: this.active_condition_content,
               content: this.active_content
           }
-          console.log(active_hash)
+          var self = this ;
+             axios.post("http://localhost:3000/actives",active_hash)
+                  .then(function(response){
+                    console.log(response)
+                  } )  
+
+
+
           }else{
-            alert('請先建立活動條件')
+            alert('請建立活動條件或是勾選活動內容')
           }
         },
       },
@@ -169,8 +179,9 @@ document.addEventListener('turbolinks:load', () => {
           },
           time_choice(){
                 let oneDay = 24 * 60 * 60 * 1000 
-                this.time_delta =Math.round(Math.abs(this.range[1] - this.range[0])/oneDay)
-                console.log(this.time_delta)
+                // this.time_delta =Math.round(Math.abs(this.range[1] - this.range[0])/oneDay)
+                
+                // console.log(this.time_delta)
                 console.log(this.range)
           },
           store_choice(){
