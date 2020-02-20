22dc3795e05b6237aca00e959d537b3665234a40
HenryYHHsu <henryhsu0529@gmail.com>
2020-02-20 10:52:50 +0800

    Create model active and active_content verison-0.0.1

--- app/controllers/actives_controller.rb
+++ app/controllers/actives_controller.rb
@@ -11,9 +11,15 @@ class ActivesController < ApplicationController
         Active.create(:name => active_name,
                       :d_target => active_key)
         puts "--------Create-----------"
-        a = Active.where(d_target: active_key).where(name: 'llllll' )
-        puts a
+        current_active = Active.where(d_target: active_key, name: active_name)
+        puts "--------result-----------"
+        current_active_id = 0 # initial 
+        current_active.map do |x| 
+            current_active_id = x.id
+        end
         puts "--------Find-----------"
+        puts current_active_id
+        puts "-----------End---------"
         for i in 0..(condition.length-1)                  
             case  condition[i][:symbol]
             when 0
@@ -25,8 +31,6 @@ class ActivesController < ApplicationController
                     condition_a_type = condition[i][:condition]
                     condition_a_limit = condition[i][:values][1]
                 end
-
-
             when 1
                 condition_b_status = condition[i][:status]
                 condition_b_target = condition[i][:values][0]
